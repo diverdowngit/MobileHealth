a61ac049b4312de7520624b3d00720e95da6e4fd
ChristinThoma <christin-thoma@t-online.de>
2021-03-28 14:04:04 +0200

    delete my-app

--- my-app/src/Components/TherapistCard.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from "react"; 
-
-export default ()=>{
-    return <Fragment>   
-                <h1>
-                    This will be a card of one Therapist
-                </h1>
-            </Fragment>
-}
--- my-app/src/Components/TherapistList.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from "react"; 
-
-export default ()=>{
-    return <Fragment>   
-                <h1>
-                    This will show all Therapist depending on your search.
-                </h1>
-            </Fragment>
-}
