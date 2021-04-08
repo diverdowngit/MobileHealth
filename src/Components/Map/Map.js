3c0291ef445bd9ed59557f679cfd95e6fbb2c952
Fred Miller <fmdiving001@gmail.com>
2021-04-08 07:27:21 +0200

    added new map

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,12 +1,12 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from "react";
 
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
+import mapboxgl from "mapbox-gl/dist/mapbox-gl-csp";
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
-
+import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
+// 🤢🤢
 mapboxgl.workerClass = MapboxWorker;
-mapboxgl.accessToken = process.env.REACT_APP_BASE_MAP
- 
+mapboxgl.accessToken = process.env.REACT_APP_BASE_MAP;
+
 const Map = () => {
   const mapContainer = useRef();
   const [lng, setLng] = useState(13.404954);
@@ -16,18 +16,18 @@ const Map = () => {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: "mapbox://styles/diverman/ckmxmpj770ywq17o3utyrki1u",
       center: [lng, lat],
-      zoom: zoom
+      zoom: zoom,
     });
     return () => map.remove();
   }, []);
 
   return (
     <div>
-      <div style={{ height: '300px', width: '300px'}} ref={mapContainer} />
+      <div style={{ height: "300px", width: "300px" }} ref={mapContainer} />
     </div>
   );
-}
+};
 
 export default Map;
