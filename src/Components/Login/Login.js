9e5dfe7a630599536c3b217716e33c1a7b727d87
Tim Schneider <timjo1@MacBook-Air.fritz.box>
2021-04-17 13:53:02 +0200

    added card border to login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,31 +1,60 @@
 import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
-import './Login.css';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCard,
+  MDBCol,
+  MDBInput,
+  MDBBtn,
+} from "mdbreact";
+import "./Login.css";
 
-const Login = ({onAuth, onSetCredentials}) => {
-return (
-<MDBContainer className="text-left md-6 mb-6" >
-  <MDBRow >
-    <MDBCol >
-      <form >
-        <br/>
-        <h2 className="text-center font-weight-bold">Login</h2>
-        <br/>
-        <div className="ilogin grey-text">
-          <MDBInput label="Type your email" name="emailAddress" icon="envelope" group type="email" validate error="wrong"
-            success="right" onChange={onSetCredentials}/>
-          <MDBInput label="Type your password" name="password" icon="lock" group type="password" onChange={onSetCredentials} validate />
-        </div>
-        <br/>
-        <div className="text-center">
-          <MDBBtn color="green" onClick={onAuth}>Login</MDBBtn>
-          <br/>
-        </div>
-      </form>
-    </MDBCol>
-  </MDBRow>
-</MDBContainer>
-);
+const Login = ({ onAuth, onSetCredentials }) => {
+  return (
+    <MDBCard className="my-5 px-5 pb-1 text-center">
+      <MDBContainer className="text-left md-6 mb-6">
+        <MDBRow>
+          <MDBCol>
+            <form>
+              <br />
+              <h2 className="text-center font-weight-bold">Login</h2>
+              <br />
+              <div className="ilogin grey-text">
+                <MDBInput
+                  label="Type your email"
+                  name="emailAddress"
+                  icon="envelope"
+                  group
+                  type="email"
+                  validate
+                  error="wrong"
+                  success="right"
+                  onChange={onSetCredentials}
+                />
+                <MDBInput
+                  label="Type your password"
+                  name="password"
+                  icon="lock"
+                  group
+                  type="password"
+                  onChange={onSetCredentials}
+                  validate
+                />
+              </div>
+              <br />
+              <div className="text-center">
+                <MDBBtn color="green" onClick={onAuth}>
+                  Login
+                </MDBBtn>
+                <br />
+              </div>
+            </form>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <br />
+    </MDBCard>
+  );
 };
 
 export default Login;
